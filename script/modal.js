8fa5f59ac6785935383c700a54ee7a4e3bbafa7a
Lisa Bureau <bureau.lisa03@gmail.com>
2025-12-15 19:54:44 +0100

    responsive design

--- script/modal.js
+++ script/modal.js
@@ -2,17 +2,22 @@ const multimedias = document.querySelectorAll(".lien-conteneur-photo");
 const modal = document.querySelector("#modal");
 const modalImg = document.querySelector("#modal-img");
 const modalVideo = document.querySelector("#modal-video");
+const modalCaption = document.querySelector("#modal-caption");
 const closeBtn = document.querySelector(".close");
 
 multimedias.forEach((multimedia) => {
     multimedia.addEventListener("click", function(event) {
         event.preventDefault();
         const url = this.href;
+        const photoHover = this.querySelector(".photo-hover");
+        const captionText = photoHover ? photoHover.textContent : "";
+
         modal.showModal();
 
         modalImg.style.display = "none";
         modalVideo.style.display = "none";
         modalVideo.pause();
+        modalCaption.textContent = captionText;
 
         if (url.match(/\.(mp4|mov)$/i)) {
             modalVideo.src = url;
--- script/navbar.js
+++ script/navbar.js
@@ -1,5 +1,7 @@
 const buttons = document.querySelectorAll(".cercle-border");
 
+buttons[1].classList.add("active");
+
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         buttons.forEach((otherButton) => {
